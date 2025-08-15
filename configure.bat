10cfb6c7d92628ea89672995766fb6d932c62d87
Maximillian Halleran <40084011+MaxHalleran@users.noreply.github.com>
2025-08-15 16:06:22 -0600

    Update configure.bat

--- configure.bat
+++ configure.bat
@@ -1,15 +1,41 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
-:: define some variable
-set SCRIPTDIR=%~dp0
-set INSTALL_DEST=%SCRIPTDIR%lizmap
-set INSTALL_SOURCE=%SCRIPTDIR%
-:: ensure it match with .env.windows !
-set QGIS_VERSION_TAG=ltr-rc
+:: ===============================
+:: Define some variables
+:: ===============================
+set "SCRIPTDIR=%~dp0"
+set "INSTALL_DEST=%SCRIPTDIR%lizmap"
+set "INSTALL_SOURCE=%SCRIPTDIR%"
 
-:: docker run that launch _configure (create service file/lizmapprofile, install plugin, ...)
-docker run -it -u 1000:1000 --rm -e INSTALL_SOURCE=/install -e INSTALL_DEST=/lizmap -e "LIZMAP_DIR=%INSTALL_DEST%" -e QGSRV_SERVER_PLUGINPATH=/lizmap/plugins -v "%INSTALL_SOURCE%:/install" -v "%INSTALL_DEST%:/lizmap" -v "%INSTALL_SOURCE%:/src" --entrypoint /src/configure.sh 3liz/qgis-map-server:%QGIS_VERSION_TAG% _configure
+:: Ensure QGIS version tag matches .env.windows
+if "%QGIS_VERSION_TAG%"=="" set "QGIS_VERSION_TAG=ltr-rc"
 
-:: all ok, next step is to launch docker-compose
+:: ===============================
+:: Convert Windows paths to Docker Unix paths
+:: Requires WSL (wsl.exe) installed
+:: ===============================
+for /f "usebackq delims=" %%i in (`wsl wslpath "%INSTALL_SOURCE%"`) do set "INSTALL_SOURCE_UNIX=%%i"
+for /f "usebackq delims=" %%i in (`wsl wslpath "%INSTALL_DEST%"`) do set "INSTALL_DEST_UNIX=%%i"
+for /f "usebackq delims=" %%i in (`wsl wslpath "%SCRIPTDIR%"`) do set "SCRIPTDIR_UNIX=%%i"
+
+:: ===============================
+:: Run Docker container
+:: ===============================
+docker run -it -u 1000:1000 --rm ^
+    -e INSTALL_SOURCE=/install ^
+    -e INSTALL_DEST=/lizmap ^
+    -e LIZMAP_DIR=/lizmap ^
+    -e QGSRV_SERVER_PLUGINPATH=/lizmap/plugins ^
+    -v "%INSTALL_SOURCE_UNIX%:/install" ^
+    -v "%INSTALL_DEST_UNIX%:/lizmap" ^
+    -v "%SCRIPTDIR_UNIX%:/src" ^
+    --entrypoint /src/configure.sh ^
+    3liz/qgis-map-server:%QGIS_VERSION_TAG% _configure
+
+:: ===============================
+:: Done
+:: ===============================
 echo setup finished, you can run 'docker-compose --env-file=.env.windows up'
- 
+
+endlocal
