b01049f1317b002bebb9080749ce8075717cfe81
Maximillian Halleran <40084011+MaxHalleran@users.noreply.github.com>
2025-08-15 17:36:21 -0600

    Replace configure.sh call with direct code

--- configure.bat
+++ configure.bat
@@ -4,56 +4,111 @@ setlocal ENABLEDELAYEDEXPANSION
 :: -------------------------------
 :: Define variables
 :: -------------------------------
-:: Make sure no trailing backslash
 set "SCRIPTDIR=%~dp0"
 if "%SCRIPTDIR:~-1%"=="\" set "SCRIPTDIR=%SCRIPTDIR:~0,-1%"
 
 set "INSTALL_DEST=%SCRIPTDIR%\lizmap"
 set "INSTALL_SOURCE=%SCRIPTDIR%"
-set "QGIS_VERSION_TAG=ltr-rc"
 
 :: Ensure install directory exists
 if not exist "%INSTALL_DEST%" mkdir "%INSTALL_DEST%"
 
 :: -------------------------------
-:: Fix line endings of configure.sh
+:: Load environment variables from env.default
 :: -------------------------------
-if exist "%SCRIPTDIR%\configure.sh" (
-    echo Converting configure.sh to LF line endings...
-    powershell -Command "(Get-Content '%SCRIPTDIR%\configure.sh') -replace \"`r`n\",\"`n\" | Set-Content -NoNewline '%SCRIPTDIR%\configure.sh'"
-) else (
-    echo [ERROR] configure.sh not found in %SCRIPTDIR%
-    exit /b 1
+for /f "usebackq tokens=1,2 delims==" %%A in ("%INSTALL_SOURCE%\env.default") do (
+    set "%%A=%%B"
 )
 
 :: -------------------------------
-:: Test mount visibility (optional debug)
+:: Create directories (mirrors _makedirs)
 :: -------------------------------
-echo Testing if configure.sh is visible inside container...
-docker run -it --rm -v "%INSTALL_SOURCE%:/src" alpine ls -l /src
-echo.
+for %%D in (
+    plugins
+    processing
+    wps-data
+    www
+    var\log\nginx
+    var\nginx-cache
+    var\lizmap-theme-config
+    var\lizmap-db
+    var\lizmap-config
+    var\lizmap-log
+    var\lizmap-modules
+    var\lizmap-my-packages
+) do (
+    if not exist "%INSTALL_DEST%\%%D" mkdir "%INSTALL_DEST%\%%D"
+)
 
 :: -------------------------------
-:: Run Lizmap Docker container with debug output
+:: Create .env file (mirrors _makenv)
 :: -------------------------------
-echo Running configure.sh inside container...
-docker run -it --rm ^
-    -u 1000:1000 ^
-    -e INSTALL_SOURCE=/install ^
-    -e INSTALL_DEST=/lizmap ^
-    -e "LIZMAP_DIR=%INSTALL_DEST%" ^
-    -e QGSRV_SERVER_PLUGINPATH=/lizmap/plugins ^
-    -v "%INSTALL_SOURCE%:/install" ^
-    -v "%INSTALL_DEST%:/lizmap" ^
-    -v "%INSTALL_SOURCE%:/src" ^
-    -v "%SCRIPTDIR%\qgis-data:/qgis-data" ^
-    3liz/qgis-map-server:%QGIS_VERSION_TAG% ^
-    sh -x /src/configure.sh _configure
+(
+echo LIZMAP_PROJECTS=%LIZMAP_PROJECTS%
+echo LIZMAP_DIR=%LIZMAP_DIR%
+echo LIZMAP_UID=%LIZMAP_UID%
+echo LIZMAP_GID=%LIZMAP_GID%
+echo LIZMAP_VERSION_TAG=%LIZMAP_VERSION_TAG%
+echo QGIS_VERSION_TAG=%QGIS_VERSION_TAG%
+echo POSTGIS_VERSION=%POSTGIS_VERSION%
+echo POSTGRES_PASSWORD=%POSTGRES_PASSWORD%
+echo POSTGRES_LIZMAP_DB=%POSTGRES_LIZMAP_DB%
+echo POSTGRES_LIZMAP_USER=%POSTGRES_LIZMAP_USER%
+echo POSTGRES_LIZMAP_PASSWORD=%POSTGRES_LIZMAP_PASSWORD%
+echo QGIS_MAP_WORKERS=%QGIS_MAP_WORKERS%
+echo WPS_NUM_WORKERS=%WPS_NUM_WORKERS%
+echo LIZMAP_PORT=%LIZMAP_PORT%
+echo OWS_PORT=%OWS_PORT%
+echo WPS_PORT=%WPS_PORT%
+echo POSTGIS_PORT=%POSTGIS_PORT%
+echo POSTGIS_ALIAS=%POSTGIS_ALIAS%
+) > "%INSTALL_DEST%\.env"
 
 :: -------------------------------
-:: Done
+:: Copy lizmap.dir content
 :: -------------------------------
+xcopy "%INSTALL_SOURCE%\lizmap.dir\*" "%INSTALL_DEST%\" /E /I /Y
+
+:: -------------------------------
+:: Create pg_service.conf (mirrors _makepgservice)
+:: -------------------------------
+if not exist "%INSTALL_DEST%\etc" mkdir "%INSTALL_DEST%\etc"
+(
+echo [lizmap_local]
+echo host=%POSTGIS_ALIAS%
+echo port=5432
+echo dbname=%POSTGRES_LIZMAP_DB%
+echo user=%POSTGRES_LIZMAP_USER%
+echo password=%POSTGRES_LIZMAP_PASSWORD%
 echo.
-echo Setup finished. You can now run:
-echo docker-compose --env-file=.env.windows up
+echo [postgis1]
+echo host=%POSTGIS_ALIAS%
+echo port=5432
+echo dbname=%POSTGRES_LIZMAP_DB%
+echo user=%POSTGRES_LIZMAP_USER%
+echo password=%POSTGRES_LIZMAP_PASSWORD%
+) > "%INSTALL_DEST%\etc\pg_service.conf"
+
+:: -------------------------------
+:: Create lizmap_local.ini.php (mirrors _makelizmapprofiles)
+:: -------------------------------
+if not exist "%INSTALL_DEST%\etc\profiles.d" mkdir "%INSTALL_DEST%\etc\profiles.d"
+(
+echo [jdb:jauth]
+echo driver=pgsql
+echo host=%POSTGIS_ALIAS%
+echo port=5432
+echo database=%POSTGRES_LIZMAP_DB%
+echo user=%POSTGRES_LIZMAP_USER%
+echo password="%POSTGRES_LIZMAP_PASSWORD%"
+echo search_path=lizmap,public
+) > "%INSTALL_DEST%\etc\profiles.d\lizmap_local.ini.php"
+
+:: -------------------------------
+:: Plugin installation placeholder
+:: -------------------------------
+echo [INFO] Please install Lizmap plugin manually or via container script: install-lizmap-plugin.sh
+
+echo.
+echo Setup finished. Files created in %INSTALL_DEST%
 endlocal
