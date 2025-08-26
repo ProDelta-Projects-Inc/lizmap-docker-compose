c4675672324d2f853427a3699915a962cdcea0dc
Maximillian Halleran <40084011+MaxHalleran@users.noreply.github.com>
2025-08-26 16:22:43 -0600

    Comment out chown command

--- configure.js
+++ configure.js
@@ -168,7 +168,7 @@ try {
     }
   }
 
-  execSync(`chown -R ${LIZMAP_UID}:${LIZMAP_GID} ${pluginsDir}`)
+  // execSync(`chown -R ${LIZMAP_UID}:${LIZMAP_GID} ${pluginsDir}`)
 
   console.log("✅ Plugins installed locally.");
 } catch (err) {
